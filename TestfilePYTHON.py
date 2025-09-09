81c07d589c1beb5822ea7c93938e61c44d3767df
JoshLDev <joshua.leon@g.austincc.edu>
2025-09-08 23:04:27 -0700

    Added square root function to return to original number ,Printed output of both and used no decimal places in the second sentance

--- TestfilePYTHON.py
+++ TestfilePYTHON.py
@@ -1,10 +1,20 @@
 # This is an example file to push to github
 
+import math
+
 def square(x):
     result = x * x
     return result
 
 
-num = square(8)
+def squareroot(x):
+    result = math.sqrt(x)
+    return result
 
-print(num)
+num = 8
+
+squarednum = square(8)
+
+print(f"The result of {num} squared is {squarednum} \n")
+print(f"The square root of {squarednum} is {squareroot(squarednum):.0f}")
+
